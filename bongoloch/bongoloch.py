d3c5587d78184c203aa05518338586810a82d762
loooph <loooph@gmx.de>
2018-10-25 23:38:56 +0200

    Added !bongoloch command

---
+++ bongoloch/bongoloch.py
@@ -0,0 +1,15 @@
+from bot import Handle
+
+
+class Bongoloch_Handle(Handle):
+
+    command = "bongoloch"
+
+    def on_message(self, bot, client, message):
+        bot.send_message("<:bongoloch:444986483145965578>")
+
+    def man(self):
+        return [
+            "usage: !bongoloch",
+            "returns bongoloch to you"
+        ]
--- handles.py
+++ handles.py
@@ -4,13 +4,15 @@ from example import example
 from restart import restart
 from man import man
 from install import install
+from bongoloch import bongoloch
 
-HANDLES  = [     
+HANDLES = [
         example.Example_Handle(),
         restart.Restart_Handle(),
         info.Info_Handle(),
         man.Man_Handle(),
-        install.Install_Handle()
+        install.Install_Handle(),
+        bongoloch.Bongoloch_Handle()
 ]
 
 
