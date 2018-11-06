969724c68024814fafea828c7881aa72432fa60c
loooph <loooph@gmx.de>
2018-11-06 20:02:50 +0100

    fix: no longer crashes because of empty file

--- deadlines/deadlines.py
+++ deadlines/deadlines.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from bot import Handle
 import pickle
 
-DATE_FILE = "dates"
+DATE_FILE = "deadlines/dates"
 
 
 class Deadlines_Handle(Handle):
@@ -18,15 +18,15 @@ class Deadlines_Handle(Handle):
         msg_array = message.split()
 
         if len(msg_array) == 0 or msg_array[0] == "list":
-            self.list_dates()
+            self.list_dates(bot)
         elif msg_array[0] == "add":
             try:
-                self.add_date("".join(msg_array[1:len(msg_array)]))
+                self.add_date(msg_array[1], msg_array[2])
             except ValueError:
                 raise Exception("Unknown argument: " + msg_array[0] + " " + msg_array[1] + ". See man " + self.command)
         elif msg_array[0] == "edit":
             try:
-                self.edit_date(int(msg_array[1]))
+                self.edit_date(int(msg_array[1]), "".join(msg_array[2:len(msg_array)]))
             except ValueError:
                 raise Exception("Unknown argument: " + msg_array[0] + " " + msg_array[1] + ". See man " + self.command)
         elif msg_array[0] == "remove":
@@ -38,27 +38,36 @@ class Deadlines_Handle(Handle):
         # TODO
         return []
 
-    def send_dates(self, bot):
-        # TODO
-        pass
-
     def save_dates(self):
         with open(DATE_FILE, "wb") as file:
+
             pickle.dump(self.dates, file, pickle.HIGHEST_PROTOCOL)
 
     def load_dates(self):
-        with open(DATE_FILE, "rb") as file:
-            self.dates = pickle.load(file)
+        try:
+            with open(DATE_FILE, "rb") as file:
+                self.dates = pickle.load(file)
+        except:
+            # TODO
+            pass
 
-    def list_dates(self):
+    def list_dates(self, bot):
         # TODO
-        pass
+        for date in self.dates:
+            bot.send_message( "ID[" + str(date["id"]) + "] MODULE: " +  date["module"] + " - DATE: " + date["date"].strftime("%d.%m.%y %H Uhr") + " |")
 
-    def add_date(self, date_string):
-        # TODO
-        pass
+    def add_date(self, date_str, module_str):
+        try:
+            date = {
+                "id": len(self.dates),
+                "date": datetime.strptime(date_str, "%d.%m.%y:%H"),
+                "module": module_str
+            }
+            self.dates.append(date)
+        except:
+            raise Exception("Wrong format:" + date_str + " " + module_str + ". See man " + self.command)
 
-    def edit_date(self, date_id):
+    def edit_date(self, date_id, date):
         # TODO
         pass
 
